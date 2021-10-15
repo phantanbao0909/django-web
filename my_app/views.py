974b05f15517678bd8a73345edc201719c4d0baf
phantanbao2009 <92307850+phantanbao2009@users.noreply.github.com>
2021-10-15 08:15:23 +0700

    manage.py file

--- my_app/views.py
+++ my_app/views.py
@@ -1,12 +1,43 @@
-from django.shortcuts import render
-from bs4 import BeautifulSoup
 import requests
-# Create your views here.
+from requests.compat import quote_plus
+from django.shortcuts import render
+from django.shortcuts import render
+from .models import Search
+from bs4 import BeautifulSoup
+from . import models
+
+BASE_CRAIGSLIST_URL = 'https://losangeles.craigslist.org/search/?query={}'
 def home(request):
     return render(request,'base.html')
+
 def new_search(request):
+    
     search  = request.POST.get('search')
-    print (search)
+    models.Search.objects.create(search=search)
+    #print(quote_plus(search))
+    final_url  = BASE_CRAIGSLIST_URL.format(quote_plus(search))
+    #print (final_url)
+    response = requests.get(final_url)
+
+    data = response.text
+    soup = BeautifulSoup(data,features = 'html.parser')
+    search = request.POST.get('search')
+    models.Search.objects.create(search=search)
+    final_url = BASE_CRAIGSLIST_URL.format(quote_plus(search))
+    response = requests.get(final_url)
+    data = response.text
+    soup = BeautifulSoup(data, features='html.parser')
+    
+    post_listings = soup.find_all('li',{'class':'result-row'})
+    post_title = post_listings[0].find(class_ = 'result-title').text
+    post_url = post_listings[0].find('a').get('href')
+    post_price = post_listings[0].find(class_ = 'result-price').text
+    
+    print (post_title)
+    print (post_price)
+    print (post_url)
+    #print (soup)
+    #print (data)
     context = {
         'search' : search
     }
